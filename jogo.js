f4d43e87426cfd36a1a31fd8b4d4778477b113f7
Evelyn dos Santos <102689284+SantosEve@users.noreply.github.com>
2022-12-12 15:34:20 -0300

    Update jogo.js

--- jogo.js
+++ jogo.js
@@ -1,17 +1,29 @@
-// Aqui teremos a programação do Flappy Bird :D
 const sprites = new Image()
 sprites.src = './sprites.png';
 
 const canvas = document.querySelector('#game-canvas');
 const contexto = canvas.getContext('2d');
 
+const flappyBird = {
+    spriteX : 0,
+    spriteY: 0,
+    largura: 35,
+    altura: 25,
+    x: 10,
+    y: 50,
+    desenha(){
+        contexto.drawImage(
+            sprites,
+            flappyBird.spriteX, flappyBird.spriteY, 
+            flappyBird.largura, flappyBird.altura,
+            flappyBird.x, flappyBird.y,
+            flappyBird.largura, flappyBird.altura,
+        );
+    }
+}
+
 function loop(){
-    contexto.drawImage(
-        sprites,
-        0, 0,
-        35, 25,
-        10, 50,
-        35, 25);
+    flappyBird.desenha();
 
         requestAnimationFrame(loop);
     }
